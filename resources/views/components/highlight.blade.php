3ac44d721ab27500f2c0b01408cb0c6fe5edd611
Josh Benham <joshbenham@gmail.com>
2023-01-26 18:03:39 +1100

    Created a couple inline blade components

---
+++ resources/views/components/highlight.blade.php
@@ -0,0 +1,3 @@
+<span class="inline-block pl-2 pr-2 border-2 rounded-lg border-fuchsia-800 bg-fuchsia-100">
+    {{ $slot }}
+</span>
---
+++ resources/views/components/paragraph.blade.php
@@ -0,0 +1,3 @@
+<p class="mb-8 text-lg leading-loose text-slate-600 md:w-10/12">
+    {{ $slot }}
+</p>
