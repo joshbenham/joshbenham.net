396edfb0c98c9a68c6a8dee335cb1b4d8ae7ce3f
Shift <shift@laravelshift.com>
2023-02-21 06:48:09 +0000

    Update method signatures for Laravel contracts

--- app/Casts/HashCast.php
+++ app/Casts/HashCast.php
@@ -2,12 +2,13 @@
 
 namespace App\Casts;
 
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Contracts\Database\Eloquent\CastsInboundAttributes;
 use Illuminate\Support\Facades\Hash;
 
 class HashCast implements CastsInboundAttributes
 {
-    public function set($model, string $key, $value, array $attributes): string|null
+    public function set(Model $model, string $key, mixed $value, array $attributes): string|null
     {
         return $value ? Hash::make($value) : $value;
     }
